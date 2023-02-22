85606c6557673412130818054bd709e0150113ae
LimeGradient <kfb13@scasd.org>
2023-02-21 23:46:30 -0500

    WIP: Unix Server and Client

--- TankUpdater+/Main.cpp
+++ TankUpdater+/Main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
+#include "Server.h"
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+    #define PATH "\\C:\\Users\\Kevin Burns\\Documents\\VERSION.tanks"
+
+#elif __APPLE__ || __linux__
+    #define PATH "Users/kevinburns/Documents/VERSION.tanks"
+	#include <arpa/inet.h>
+	#include <sys/socket.h>
+	#include <unistd.h>
+#endif
 
 std::string version = "1.0";
 
@@ -8,22 +20,23 @@ bool isLatestVersion(std::string latestVersion) {
 	using namespace std;
 
 	string content;
-	ifstream file("\\C:\\Users\\Kevin Burns\\Documents\\VERSION.tanks");
+	ifstream file(PATH);
 	while (getline(file, content)) {
-		if (content == latestVersion) {
-			return true;
-		}
-		else {
+		if (content != latestVersion) {
 			return false;
 		}
 	}
 	file.close();
+	return true;
 }
 
 int main() {
 	if (isLatestVersion("1.0")) {
-		std::cout << "Latest Version Installed" << std::endl;
-		return 0;
+		UnixServer us;
+		us.StartClient();
+	} else {
+		std::cout << "Needs Update" << std::endl;
+		return -1;
 	}
 	return -1;
 }
--- TankUpdater+/Server.h
+++ TankUpdater+/Server.h
@@ -1,6 +1,24 @@
 #pragma once
-class Server
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+    #include <WinSock2.h>
+    #include <WS2tcpip.h>
+
+#elif __APPLE__ || __linux__
+    #include <netinet/in.h>
+    #include <unistd.h>
+    #include <sys/socket.h>
+
+#endif
+class WinServer
 {
+public:
 	int StartServer();
+	int StartClient();
 };
 
+class UnixServer
+{
+public:
+	int StartServer();
+	int StartClient();
+};
